4d32a743c04fd2baa7db058014c42e55a7c0c20e
DESKTOP-ERMH4GV\DELL <scottnguyen2411@gmail.com>
2023-05-05 11:48:43 +0700

    Add images

---
+++ ecommerce/src/product.js
@@ -0,0 +1,52 @@
+import product1 from './assets/Cha Bong.jpg';
+import product2 from './assets/chà bông.jpg';
+import product3 from './assets/Chả chiên.jpg';
+import product4 from './assets/cha chien2.jpg';
+import product5 from './assets/cha hoa trinh bay.jpg';
+import product6 from './assets/chả hoa.jpg';
+import product7 from './assets/chả quế.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Chà Bông",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Chà Bông 2",
+        price: 999.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Chả Chiên",
+        price: 999.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Chả Chiên 2",
+        price: 999.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Chả Hoa",
+        price: 999.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Chả Hoa 2",
+        price: 999.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Chả Quế",
+        price: 999.0,
+        productImage: product7,
+    },
+]
